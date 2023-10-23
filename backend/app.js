71e99f0d101fb7140171abf5622d7cec8f7e754a
Lucas dos Santos <83959238+Lucky3ds@users.noreply.github.com>
2023-10-23 14:51:52 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const express = require("express")
 const path = require("path")
 const cors = require("cors")
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
 const app = express(); 
 
